77968a437c30e63b2322a618e77648db2586d03b
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-02 19:26:10 +0800

    create MySQL database

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize')
+
+const sqlize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: { charset: 'utf8mb4' }
+  }
+);
+
+module.exports = sqlize
