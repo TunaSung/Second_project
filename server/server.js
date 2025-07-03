86232996763879237125229fec0a337fd0108968
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-04 03:05:31 +0800

    adjust cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,10 @@ const eventRoute = require('./routes/eventRoute')
 const sqlize = require('./config/database')
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
