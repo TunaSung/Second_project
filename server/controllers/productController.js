8ed692c49021eba61abae1279a2c0b2a0d247183
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-03 15:03:12 +0800

    add getMyshop

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,8 +1,9 @@
 const { Product } = require('../models/Association')
+const authenticate = require('../middleware/JWT');
 
 exports.getAllProudcts = async (req, res) => {
     try {
-        const products = await Product.findAll();
+        const products = await Product.findAll({where: {isAvailable: true}});
         res.status(200).json({products})
     } catch (error) {
         res.status(500).json({error: "fetch products failed", details: error.message})
@@ -23,16 +24,35 @@ exports.getOneProudct = async (req, res) => {
     }
 }
 
+exports.getMyShop = async (req, res) => {
+    try {
+        const userId = req.user?.id || 1
+
+        const product = await Product.findAll({where: {sellerId: userId}});
+        if(product){
+            res.status(201).json({product});
+        } else {
+            res.status(404).json({ message: "Can't find product" });
+        }
+    } catch (error) {
+        res.status(500).json({error: "fetch products failed", details: error.message}) 
+    }
+}
+
+
+
 exports.uploadProudct = async (req, res) => {
     try {
-        const {name, price, stock, hashTags, image} = req.body
+        const {name, price, stock, hashTags, imageUrls} = req.body
+        const userId = req.user?.id || 1
         
         await Product.create({
             name: name,
             price: price,
             stock: stock,
             hashTags: hashTags,
-            image: image
+            imageUrls: imageUrls,
+            sellerId: userId
         })
 
         res.status(200).json({message: "Upload successfully"})
