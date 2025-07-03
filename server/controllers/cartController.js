1d65e85e883dda4cfda962f554b28a64db7c525f
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-03 15:08:30 +0800

    new controller

---
+++ server/controllers/cartController.js
@@ -0,0 +1,138 @@
+const { Product } = require('../models/Association')
+const { ProductInOrder } = require('../models/Association')
+const { Order } = require('../models/Association')
+const authenticate = require('../middleware/JWT')
+const { where } = require('sequelize')
+
+exports.addToCart =  async (req, res) => {
+    try {
+        const {productId, amount} = req.body
+        const userId = req.user?.id || 1
+
+        let order = await Order.findOne({
+            where: {
+                userId: userId,
+                status: "pending",
+                merchantTradeNo: null
+            }
+        })
+
+        if(!order){
+            order = await Order.create({
+                userId: userId,
+                status: "pending",
+                merchantTradeNo: null
+            })
+        }
+        
+        const productInOrder = await ProductInOrder.findOne({
+            where: {
+                orderId: order.id,
+                productId: productId
+            }
+        })
+
+        if(productInOrder){
+            productInOrder.amount += amount
+            await productInOrder.save() 
+        }else{
+            await ProductInOrder.create({
+                orderId: order.id,
+                productId: productId,
+                amount: amount
+            })
+        }
+
+        res.status(200).json({message: "Add successfully"})
+    } catch (error) {
+        res.status(500).json({error: "Add failed", details: error.message})
+    }
+}
+
+exports.getCart = async (req, res) => {
+    try {
+        const userId = req.user?.id || 1
+
+        const order = await Order.findOne({
+            where: {
+            userId: userId,
+            status: "pending",
+            merchantTradeNo: null
+            },
+            include: [{
+                model: ProductInOrder,
+                attributes: ["id", "amount"],
+                include: [{
+                    model: Product,
+                    attributes: ["id", "name", "price", "imageUrls", "hashTags"]
+                }]
+            }]
+        })
+
+        if(!order){
+            return res.status(400).json({message: "Get failed"})
+        }else{
+            return res.status(200).json({message: "Get successfully", order})
+        }
+
+    } catch (error) {
+        res.status(500).json({error: "Get failed", details: error.message})
+    }
+}
+
+exports.updateAmount = async (req, res) => {
+    try {
+        const { productId, amount } = req.body;
+        const userId = req.user?.id || 1
+
+        const order = await Order.findOne({
+            where:{
+                userId: userId,
+                status: "pending",
+                merchantTradeNo: null
+            }
+        })
+
+        const productInOrder = await ProductInOrder.findOne({
+            where: {
+                orderId: order.id,
+                productId: productId
+            }
+        })
+
+        productInOrder.amount = amount
+        await productInOrder.save()
+
+        res.status(200).json({message: "update successfully"})
+    } catch (error) {
+        res.status(500).json({error: "update amount failed", details: error.message})
+    }
+}
+
+exports.deleteItem = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const userId = req.user?.id || 1
+
+        const order = await Order.findOne({
+            where:{
+                userId: userId,
+                status: "pending",
+                merchantTradeNo: null
+            }
+        })
+
+        const productInOrder = await ProductInOrder.findOne({
+            where: {
+                orderId: order.id,
+                productId: productId
+            }
+        })
+
+        await productInOrder.destroy()
+
+        res.status(200).json({message: "delete successfully"})
+    } catch (error) {
+        res.status(500).json({error: "delete item failed", details: error.message})
+    }
+}
---
+++ server/controllers/eventController.js
@@ -0,0 +1,21 @@
+const { Event } = require('../models/Association');
+const { Op } = require('sequelize');
+
+exports.getEvents = async (req, res) => {
+    try {
+        const now = new Date();
+        const events = await Event.findAll({
+            where: {
+                endDate:   { [Op.gt]:  now } // 結束時間大於現在
+            },
+            order: [['endDate', 'ASC']], // 依結束時間排序近到遠(ASC 生冪排列) //反過來是 DESC 降冪排列 
+            attributes: ['id', 'title','description','imageUrl','startDate','endDate'],
+            limit: 8,
+        });
+        res.status(200).json({ events });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "獲取活動資料失敗", error });
+    }
+  };
+
