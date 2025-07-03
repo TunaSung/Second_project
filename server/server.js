9d3dad69f44244b7a3a1cee597377b4ca629827c
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-03 15:07:58 +0800

    add path of event and cart

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const authRoute = require('./routes/authRoute')
 const productRoute = require('./routes/productRoute')
+const cartRoute = require('./routes/cartRoute')
+const eventRoute = require('./routes/eventRoute')
 const sqlize = require('./config/database')
 
 const app = express()
@@ -10,6 +12,8 @@ app.use(express.json())
 
 app.use('/api/auth', authRoute)
 app.use('/api/product', productRoute)
+app.use('/api/cart', cartRoute)
+app.use('/api/event', eventRoute)
 
 sqlize.sync().then(() => {
     console.log("資料庫已同步");
