968b9a01781b73907c3c178950c5065986628613
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-05 22:33:12 +0800

    add authContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './components/Context/authContext'
+import AuthProvider from './components/Context/authContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
