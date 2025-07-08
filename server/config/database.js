08d7a28ac9ba3a6beebe235682785fb2f0b5ce85
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-08 16:42:29 +0800

    變更環境

--- server/config/database.js
+++ server/config/database.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize')
 
-const sqlize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-    dialectOptions: { charset: 'utf8mb4' }
-  }
-);
+// const sqlize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//     logging: false,
+//     dialectOptions: { charset: 'utf8mb4' }
+//   }
+// );
+
+const sqlize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: { charset: 'utf8mb4' }
+});
 
 module.exports = sqlize
--- server/server.js
+++ server/server.js
@@ -1,31 +1,60 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const authRoute = require('./routes/authRoute')
 const productRoute = require('./routes/productRoute')
 const cartRoute = require('./routes/cartRoute')
 const eventRoute = require('./routes/eventRoute')
+const paymentRoutes = require("./routes/paymentRoutes");
 const sqlize = require('./config/database')
 const path = require('path')
+const bodyParser = require('body-parser');
 
 const app = express()
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+const origins = (process.env.CORS_ORIGINS || "http://localhost:5173")
+  .split(",");
+
+const corsOptions = {
+  origin: origins,
+  credentials: true, // ← 這行會讓 res header 帶上 Access-Control-Allow-Credentials: true
+};
+app.use(cors(corsOptions)); // 全域套用
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
+
 app.use('/api/auth', authRoute)
+
 app.use('/api/product', productRoute)
+
 app.use('/api/cart', cartRoute)
+
 app.use('/api/event', eventRoute)
+
+app.use('/api/payment', bodyParser.urlencoded({ extended: true }));
+app.use('/api/payment', bodyParser.json());
+app.use('/api/payment', paymentRoutes);
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
 
 sqlize.sync().then(() => {
     console.log("資料庫已同步");
 });
 
-const PORT = 3000
-app.listen(PORT, ()=>{
+// if (process.env.NODE_ENV === "production") {
+//   const staticPath = path.join(__dirname, "../frontend/dist");
+//   console.log("serve 靜態檔案：", staticPath);
+//   app.use(express.static(staticPath));
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.join(staticPath, "index.html"))
+//   );
+// }
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", ()=>{
     console.log(`伺服器運行在 http://localhost:${PORT}`);
 })
