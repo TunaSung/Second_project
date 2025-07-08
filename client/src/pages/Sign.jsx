3cfc9a17b4061a71a97db52864310bca5804cf18
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-08 17:24:10 +0800

    調整標題與按鈕的顏色及排版

--- client/src/pages/Sign.jsx
+++ client/src/pages/Sign.jsx
@@ -31,27 +31,19 @@ function Sign() {
                 >
                     {/* start title */}
                     <div className="w-full mb-60">
-                        <div className="border pl-2 flex justify-start text-7xl mb-3 sail-regular 
-                        bg-clip-text text-transparent drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set"
-                        style={{
-                            backgroundImage: 'url("/imgs/kpop/aespa-winter-hot-mess.jpg")'
-                        }}>
+                        <div className="pl-2 flex justify-start text-7xl mb-3 sail-regular text-[#D2D0A0]
+                        drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set">
                             Sign In &
                         </div>
-                        <div className="border pl-2 flex justify-end text-7xl mb-3 sail-regular 
-                        bg-clip-text text-transparent drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set"
-                        style={{
-                            backgroundImage: 'url("/imgs/kpop/aespa-winter-hot-mess.jpg")'
-                        }}>
+                        <div className="pl-2 flex justify-end text-7xl mb-3 sail-regular text-[#aeac80]
+                        drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set">
                             Rediscover Value
                         </div>
                     </div>
                     {/* end title */}
                     
                     {/* start btn */}
-                    <button className="w-40 h-20 border-2 border-gray-500 rounded-xl opacity-85 group text-2xl font-bold pacifico-regular
-                    bg-gradient-to-r from-[#fef6e4] via-[#c584ba] to-[#6da0db] drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)]
-                    hover:opacity-100 hover:border-pink-400 hover:scale-110 hover:text-pink-600 transition-all duration-250"
+                    <button className="w-40 h-20 border-2 border-gray-500 bg-[#73946B] text-[#D2D0A0] rounded-xl opacity-85 text-2xl font-bold hover:scale-110 hover:opacity-100 hover:bg-[#537D5D] transition-all duration-250"
                     onClick={toggleSignChange}
                     >
                         Go Sign Up
@@ -71,27 +63,19 @@ function Sign() {
                 >
                     {/* start title */}
                     <div className="w-full mb-60">
-                        <div className="border pl-2 flex justify-start text-7xl mb-3 sail-regular 
-                        bg-clip-text text-transparent drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set"
-                        style={{
-                            backgroundImage: 'url("/imgs/kpop/aespa-winter-hot-mess.jpg")'
-                        }}>
+                        <div className="pl-2 flex justify-start text-7xl mb-3 sail-regular text-[#D2D0A0]
+                        drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-cover-set">
                             Sign Up &
                         </div>
-                        <div className="border pl-2 flex justify-end text-7xl mb-3 sail-regular 
-                        bg-clip-text text-transparent drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-contian-set"
-                        style={{
-                            backgroundImage: 'url("/imgs/kpop/aespa-winter-hot-mess.jpg")'
-                        }}>
+                        <div className="pl-2 flex justify-end text-7xl mb-3 sail-regular text-[#aeac80]
+                        drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)] bg-contian-set">
                             Discover Gems
                         </div>
                     </div>
                     {/* end title */}
 
                     {/* start btn */}
-                    <button className="w-40 h-20 border-2 border-gray-500 rounded-xl opacity-85 group text-2xl font-bold pacifico-regular
-                    bg-gradient-to-br from-[#fef6e4] via-[#599be2] to-[#e487ce] drop-shadow-[3px_3px_3px_rgba(0,0,0,0.8)]
-                    hover:opacity-100 hover:border-sky-400 hover:scale-110 hover:text-sky-900 transition-all duration-250"
+                    <button className="w-40 h-20 border-2 border-gray-500 bg-[#73946B] text-[#D2D0A0] rounded-xl opacity-85 text-2xl font-bold hover:scale-110 hover:opacity-100 hover:bg-[#537D5D] transition-all duration-250"
                     onClick={toggleSignChange}
                     >
                         Go Sign In
