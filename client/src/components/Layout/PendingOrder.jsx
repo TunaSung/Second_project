cd6c2c4584d06d0ab0680d489c4e4a24abcfff56
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-12 19:55:52 +0800

    新增 HistoryItem 和 PendingOrder 組件，整合訂單顯示功能

---
+++ client/src/components/Layout/PendingOrder.jsx
@@ -0,0 +1,14 @@
+import HistoryItem from '../Feature/HistoryItem'
+
+function PendingOrder({ merchantTradeNo, date, pios, seller }) {
+    return (
+        <div className="w-full border rounded-xl overflow-hidden drop-shadow-[2px_2px_3px_rgba(0,0,0,0.8)]">
+            {pios.map((item, i) => (
+                <HistoryItem key={i} merchantTradeNo={merchantTradeNo} amount={item.amount} image={item.product.imageUrls} 
+                name={item.product.name} price={item.product.price} date={date} seller={seller} />
+            ))}
+        </div>
+    )
+}
+
+export default PendingOrder
