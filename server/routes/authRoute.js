484c7ce7ff4c8a24e5ea063bad054ca1dcfe1dca
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-07-28 00:17:44 +0800

    新增用id找user資料

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { getUserInfo, signUp, signIn, updateProfileInfo, updateCreditCard, updateAvatarUrl } = require('../controllers/authController')
+const { getUserInfo, getUserById, signUp, signIn, updateProfileInfo, updateCreditCard, updateAvatarUrl } = require('../controllers/authController')
 const authenticate = require('../middleware/JWT');
 
 const router = express.Router()
 router.get('/', authenticate, getUserInfo)
+router.get('/:id', getUserById)
 router.post('/sign-up', signUp)
 router.post('/sign-in', signIn)
 router.put('/update/info', authenticate, updateProfileInfo)
