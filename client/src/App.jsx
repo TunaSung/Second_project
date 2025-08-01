da127a921ce3e2b56125aaffae8ed5ead95851c4
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-08-01 16:31:25 +0800

    防呆

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,88 +1,99 @@
-import './App.css'
-import NavbarFixed from './components/Layout/NavbarFixed'
-import Navbar from './components/Layout/Navbar'
-import Footer from './components/Layout/Footer'
-import FixedMessage from './components/Feature/FixedMessage'
-import { useState, useEffect, lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { clearToken, setAuthHeader } from './services/authService' 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'ldrs/dotStream'
+import "./App.css";
+import NavbarFixed from "./components/Layout/NavbarFixed";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import FixedMessage from "./components/Feature/FixedMessage";
+import { useEffect, lazy, Suspense } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { clearToken, setAuthHeader } from "./services/authService";
+import { useAuth } from "./components/Context/authContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "ldrs/dotStream";
 
-const Home = lazy(() => import('./pages/Home')); 
-const Product = lazy(() => import('./pages/Product'));
-const Sign = lazy(() => import('./pages/Sign'))
-const Personal = lazy(() => import('./pages/Personal'))
-const MyShop = lazy(() => import('./pages/MyShop'))
-
+const Home = lazy(() => import("./pages/Home"));
+const Product = lazy(() => import("./pages/Product"));
+const Sign = lazy(() => import("./pages/Sign"));
+const Personal = lazy(() => import("./pages/Personal"));
+const MyShop = lazy(() => import("./pages/MyShop"));
 
 // Dynamically adjust navbar
-function DynamicNavbar() { 
+function DynamicNavbar() {
   const location = useLocation();
-  
+
   if (location.pathname === "/") {
     return <NavbarFixed />;
   }
   return <Navbar />;
 }
 
- function ScrollToTop() {
-    const location = useLocation()
+function ScrollToTop() {
+  const location = useLocation();
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [location]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-    return null;
-  }
+  return null;
+}
 
 function App() {
+  const { categories } = useAuth();
 
   useEffect(() => {
-    setAuthHeader()
-  },[])
+    setAuthHeader();
+  }, []);
 
   useEffect(() => {
-    const EXPIRE_MS = 60*60*1000 // 一小時
-    const ts = Number(localStorage.getItem('tokenSaveAt'))
-    const now = Date.now() // 頁面刷新後會重算
+    const EXPIRE_MS = 60 * 60 * 1000; // 一小時
+    const ts = Number(localStorage.getItem("tokenSaveAt"));
+    const now = Date.now(); // 頁面刷新後會重算
 
-    if(!ts || now-ts > EXPIRE_MS){ //過一小時後自動
-      clearToken()
-    }else{
+    if (!ts || now - ts > EXPIRE_MS) {
+      //過一小時後自動
+      clearToken();
+    } else {
       const timeout = setTimeout(() => {
-        clearToken()
-      }, EXPIRE_MS - (now-ts))
+        clearToken();
+      }, EXPIRE_MS - (now - ts));
 
-      return () => clearTimeout(timeout) // 刷新時就取消計時器，避免記憶體浪費
+      return () => clearTimeout(timeout); // 刷新時就取消計時器，避免記憶體浪費
     }
-  }, [])
+  }, []);
+
+  if (categories.length === 0) {
+    return <p className="text-center my-10 text-gray-400">載入中...</p>;
+  }
 
   return (
     <Router>
-      <ScrollToTop/>
+      <ScrollToTop />
       <ToastContainer position="top-right" autoClose={3000} />
-      <FixedMessage/>
+      <FixedMessage />
       <DynamicNavbar />
-      <Suspense fallback={<div className="w-full h-[50vh] flex items-center justify-center text-center my-60"> 
-                            <l-dot-stream
-                            size="60"
-                            speed="2.5"
-                            color="black" 
-                            ></l-dot-stream>
-                        </div>}>
+      <Suspense
+        fallback={
+          <div className="w-full h-[50vh] flex items-center justify-center text-center my-60">
+            <l-dot-stream size="60" speed="2.5" color="black"></l-dot-stream>
+          </div>
+        }
+      >
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/product" element={<Product />}/>
-          <Route path="/sign" element={<Sign />}/>
-          <Route path="/personal" element={<Personal />}/>
-          <Route path="/my-shop" element={<MyShop />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/sign" element={<Sign />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/my-shop" element={<MyShop />} />
         </Routes>
       </Suspense>
-      <Footer/>
+      <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
