3df4be763fbc6bd3a12073e0fe8c8e54a5e804a1
TunaSung <125634944+TunaSung@users.noreply.github.com>
2025-11-11 14:01:21 +0800

    格式化文件

--- client/src/pages/4_Personal/component/layout/CompletedOrder.jsx
+++ client/src/pages/4_Personal/component/layout/CompletedOrder.jsx
@@ -1,15 +1,22 @@
-import HistoryItem from '../feature/HistoryItem'
+import HistoryItem from "../feature/HistoryItem";
 
 function CompletedOrder({ merchantTradeNo, date, pios, seller }) {
-
-    return (
-        <div className="w-full border border-slate-400 rounded-xl overflow-hidden shadow-md">
-            {pios.map((item, i) => (
-                <HistoryItem key={i} merchantTradeNo={merchantTradeNo} amount={item.amount} image={item.product.imageUrls} 
-                name={item.product.name} price={item.product.price} date={date} seller={seller} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="w-full border border-slate-400 rounded-xl overflow-hidden shadow-md">
+      {pios.map((item, i) => (
+        <HistoryItem
+          key={i}
+          merchantTradeNo={merchantTradeNo}
+          amount={item.amount}
+          image={item.product.imageUrls}
+          name={item.product.name}
+          price={item.product.price}
+          date={date}
+          seller={seller}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default CompletedOrder
+export default CompletedOrder;
--- client/src/pages/4_Personal/component/layout/PendingOrder.jsx
+++ client/src/pages/4_Personal/component/layout/PendingOrder.jsx
@@ -1,14 +1,22 @@
-import HistoryItem from '../feature/HistoryItem'
+import HistoryItem from "../feature/HistoryItem";
 
 function PendingOrder({ merchantTradeNo, date, pios, seller }) {
-    return (
-        <div className="w-full border border-slate-300 rounded-xl overflow-hidden shadow-md">
-            {pios.map((item, i) => (
-                <HistoryItem key={i} merchantTradeNo={merchantTradeNo} amount={item.amount} image={item.product.imageUrls} 
-                name={item.product.name} price={item.product.price} date={date} seller={seller} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="w-full border border-slate-300 rounded-xl overflow-hidden shadow-md">
+      {pios.map((item, i) => (
+        <HistoryItem
+          key={i}
+          merchantTradeNo={merchantTradeNo}
+          amount={item.amount}
+          image={item.product.imageUrls}
+          name={item.product.name}
+          price={item.product.price}
+          date={date}
+          seller={seller}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default PendingOrder
+export default PendingOrder;
